0bf09504ce1fc34a7df99f2f490d4d8faf4a0bc2
Eriasuitor <funtree9@icloud.com>
2018-03-13 17:30:26 +0800

    init web vue

---
+++ vue_web/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <HelloWord></HelloWord>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import HelloWord from './components/HelloWorld'
+export default {
+  components: {HelloWord},
+  name: 'App'
+}
+</script>
+
+<style>
+  @import "./style/style.scss";
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue_web/src/page/content.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>Content</div>
+</template>
+<script>
+export default {
+  created(){
+      console.log(this.$route)
+      console.log(this.$route.params.id)
+      this.$router.push('/')
+  }
+}
+</script>
